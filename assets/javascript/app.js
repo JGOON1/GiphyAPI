cadeb04b22094ab07128eda8370dcb3e9e85e5fc
john Meng <john.meng014@gmail.com>
2019-09-19 13:01:14 -0400

    created a function to creat buttons of the topics. got it to show in the buttons div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,36 @@
-$(document).ready(function(){
+
+    var topics = ["sports", "games"];
+
+    function displayGifInfo(){
+        var topic = $(this)("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=m46gXewIiFo3RsVZ8LNdLkCjla0ViBvT"
+
+
+    }
+
+    function renderButtons(){
+        $("#buttons-view").empty();
+        for (var i = 0; i < topics.length; i++){
+            var a = $("<button>");
+            a.addClass("topic");
+            a.attr("data-name", topics[i]);
+            a.text(topics[i]);
+            $("#buttons-view").append(a);
+            
+        }
+    }
+
+    // adding a button and pushing it to topics
+    $("#add-gif").on("click", function(event){
+        event.preventDefault();
+
+        var topic = $("#gif-input").val().trim();
+        topics.push(topic);
+        renderButtons();
+
+    });
+
+    renderButtons();
 
 
 
-});
