c88d4df224af3e5b74bd58ce9fc69baa3b526e71
john Meng <john.meng014@gmail.com>
2019-09-19 16:27:46 -0400

    was able to add function for what ever is submitted the button gets added to the buttton div. before couldn't get it to work.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,60 @@
 
-    var topics = ["sports", "games"];
+var topics = ["sports", "games"];
 
-    function displayGifInfo(){
-        var topic = $(this)("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=m46gXewIiFo3RsVZ8LNdLkCjla0ViBvT"
+$("button-view").on("click", function () {
 
 
-    }
+    var topic = $(this)("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=&api_key=m46gXewIiFo3RsVZ8LNdLkCjla0ViBvTlimit=10&offset=0&rating=G&lang=en&q=" + topic;
 
-    function renderButtons(){
-        $("#buttons-view").empty();
-        for (var i = 0; i < topics.length; i++){
-            var a = $("<button>");
-            a.addClass("topic");
-            a.attr("data-name", topics[i]);
-            a.text(topics[i]);
-            $("#buttons-view").append(a);
-            
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 0; i < 10; i++) {
+            var image = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+            // attaching data-still and non-animated gif to the element
+            image = image.attr("data-still", response.data[i].images.fixed_height_still.url);
+            // attaching data-animate and an animated gif to the element
+            image = image.attr("data-animate", response.data[i].images.fixed_height_downsampled.url)
+            // adding class "gif" to the element
+            image = image.addClass("gif");
+
+            image = image.attr("data-state", "still");
+            //appending the image and the rating below the images
+            $("#myStuff").append(image).append("<p> Rating: " + response.data[i].rating + "</p>");
         }
-    }
-
-    // adding a button and pushing it to topics
-    $("#add-gif").on("click", function(event){
-        event.preventDefault();
-
-        var topic = $("#gif-input").val().trim();
-        topics.push(topic);
-        renderButtons();
-
     });
+})
 
+
+
+
+function renderButtons() {
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+        a.addClass("topic-btn");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#buttons-view").append(a);
+
+    }
+}
+
+// adding a button and pushing it to topics
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
+
+    var topic = $("#gif-input").val().trim();
+    topics.push(topic);
     renderButtons();
+});
+// $(document).on("click", ".topic-btn", displayGifInfo);
+
+renderButtons();
+    
 
 
 
